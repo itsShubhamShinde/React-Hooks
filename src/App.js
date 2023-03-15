f7a964ec44a9923097de020709e4df7492a7bf48
itsShubhamShinde <itsshubhamks@gmai.com>
2023-03-15 18:25:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
+import { Data } from './Components/Data.js'
 
 function App() {
+  const [questionnum, setQuestionnum] = useState(0);
+  const [score, setScore] = useState(0)
+  const [showScore, setShowScore] = useState(false)
+  const handelState = (type) => {
+
+    if (type) {
+      setScore(score + 1)
+    }
+    const nextvalue = questionnum + 1
+    if (nextvalue < Data.length) {
+
+      setQuestionnum(nextvalue)
+    }
+    else {
+      setShowScore(true)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {showScore ? (
+        <div className="OuterScore">
+          <h1> Score is {score} out of 5</h1>
+          <p>Answer</p>
+          <div className="showScoreHeader">
+            {
+              Data.map((ele) => {
+                return <div>
+                  <h3 style={{ color: "#7a007a" }}>{ele.question}</h3>
+                  {ele.answer.map((ele) => {
+                    if (ele.type === true) {
+                      return <p style={{ paddingLeft: "20px" }}> - {ele.option}</p>
+                    }
+                    return null;
+                  })}
+                </div>
+              })
+            }
+          </div>
+        </div>
+
+      ) : (
+
+        <div className='Outer'>
+          <div className='headerDiv'>
+            <h2 style={{ color: "green", fontSize: "2rem" }}>Geekstar Quiz</h2>
+            <p>Curront Score: {score} </p>
+          </div>
+          <div className='questionDiv'>
+            <h3 style={{ color: "white" }}>Question {questionnum + 1} out of 5</h3>
+            <h2 style={{ color: "#e9b74e" }} >{Data[questionnum].question}</h2>
+            {Data[questionnum].answer.map((ele) => {
+              return <button onClick={() => {
+                handelState(ele.type)
+              }}>
+                {ele.option}</button>
+            })}
+          </div>
+        </div>)}
+
     </div>
   );
 }
---
+++ src/Components/Data.js
@@ -0,0 +1,47 @@
+export const Data =[
+    {
+        question : "What is the Capital of India?",
+        answer:[
+            {option :"Delhi" , type: true},
+            {option :"Mumbai" , type: false},
+            {option :"Channai" , type: false},
+            {option :"Thane" , type: false},
+        ]
+    },
+    {
+        question : "Who is the Prime Minister of India?",
+        answer:[
+            {option :"Amit Shah" , type: false},
+            {option :"Narendra Modi" , type: true},
+            {option :"Yogi Adityanath" , type: false},
+            {option :"Atal Bihari Vajpayee" , type: false},
+        ]
+    },
+    {
+        question : "Who is the founder of Tata Group?",
+        answer:[
+            {option :"Ratan Tata" , type: false},
+            {option :"Naval Tata" , type: false},
+            {option :"Jamsetji Tata" , type: true},
+            {option :"J. R. D. Tata" , type: false},
+        ]
+    },
+    {
+        question : "Who was the first Indian to go to space?",
+        answer:[
+            {option :"Vikram Ambalal" , type: false},
+            {option :"Ravish Malhotra" , type: false},
+            {option :"Nagapathi Bhat" , type: false},
+            {option :"Rakesh Sharma" , type: true},
+        ]
+    },
+    {
+        question : "Who wrote the Indian National Anthem?",
+        answer:[
+            {option :"Rabindranath Tagore" , type: true},
+            {option :"Bakim Chandra Chatterji" , type: false},
+            {option :"Swami Vivekanand" , type: false},
+            {option :"Dr. B R Ambedkar" , type: false},
+        ]
+    },
+]
